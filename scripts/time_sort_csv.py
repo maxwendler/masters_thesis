ca3bbb3ddb660b526528de24666693e31b602859
s3997128--tu-dresden.de <max.wendler@mailbox.tu-dresden.de>
2023-11-13 11:03:22 +0100

    implemented sorting of complete coordinate result csvs in Snakemake

--- scripts/time_sort_csv.py
+++ scripts/time_sort_csv.py
@@ -2,33 +2,30 @@ import argparse
 import csv
 from operator import itemgetter
 
-parser = argparse.ArgumentParser(prog="plot_dists.py", description="Sorts csv after columns node (1st) and time (2nd).")
+parser = argparse.ArgumentParser(prog="time_sort_csv.py", description="Sorts csv after columns node (1st) and time (2nd).")
 
 parser.add_argument("csv_path", help="Path of the csv of simulation results.")
 
 args = parser.parse_args()
 
 rows = []
+print(f"reading {args.csv_path} ...")
 with open(args.csv_path) as csv_f:
     dict_reader = csv.DictReader(csv_f, delimiter="\t")
     
     for row in dict_reader:
         rows.append( tuple( [row["node"], [int(row["time"])], row] ) )
 
-print(len(rows))
-# sort by node str and time in
+# sort by (1) node str and (2) time
 rows.sort(key=itemgetter(0, 1))
-print(len(rows))
 # get actual rows from sorted tuples and add header
 rows = [r[2] for r in rows]
-print(len(rows))
 header = "\t".join(rows[0].keys()) + "\n"
 rows = [ ("\t".join(r.values()) + "\n") for r in rows]
 rows = [header] + rows
-print(len(rows))
-# turn dicts from DictReader to str
 
-print(rows[:10])
-
-with open(args.csv_path.removesuffix(".csv") + "_sorted.csv", "w") as new_csv_f:
+sorted_path = args.csv_path.removesuffix(".csv") + "_sorted.csv"
+with open(sorted_path, "w") as new_csv_f:
     new_csv_f.writelines(rows)
+
+print(f"done ({sorted_path})")
