b8fb68b7e0e5aaeab8e82a65da18b00c5459509e
s3997128--tu-dresden.de <max.wendler@mailbox.tu-dresden.de>
2023-12-06 15:35:26 +0100

    plotting zenith shifts

---
+++ scripts/plots/plot_const_period_zenith_shifts.py
@@ -0,0 +1,72 @@
+import argparse
+import json
+import os
+import plotly.graph_objects as go
+
+parser = argparse.ArgumentParser(prog="plot_const_period_zenith_shifts",
+                                 description="""Plots zenith shifts of overlapping communication periods of two mobilities for all satellite modules
+                                 of a constellation as a function of time distance to the used TLE's epoch.""")
+parser.add_argument("comm_comp_dir", help="Directory with all the communication comparison JSONs of a constellation.")
+parser.add_argument("svg_output_path")
+parser.add_argument("--differences", action="store_true", help="If flag is set, plots absolute values of zenith shifts.")
+args = parser.parse_args()
+
+comm_comp_dir = args.comm_comp_dir if args.comm_comp_dir.endswith("/") else args.comm_comp_dir + "/"
+ref_mobility = None 
+new_mobility = None
+
+# get data as (offset to epoch, zenith shift, modname) tuples
+data = []
+for comm_comp_fname in filter(lambda fname: fname.endswith("communication_comparison.json"), os.listdir(comm_comp_dir)):
+    
+    if not ref_mobility:
+        mobilties = comm_comp_fname.split("_")[0].split("-")
+        ref_mobility = mobilties[0]
+        new_mobility = mobilties[1]
+
+    comm_comp_path = comm_comp_dir + comm_comp_fname
+    with open(comm_comp_path, "r") as json_f:
+        comm_comp = json.load(json_f)
+    
+    for p_group in comm_comp["period_groups"]:
+        if args.differences:
+            data.append( tuple( [p_group["ref_start_to_epoch_offset"], abs(p_group["zenith_shift"]), comm_comp["modname"]] ) )
+        else:
+            data.append( tuple( [p_group["ref_start_to_epoch_offset"], p_group["zenith_shift"], comm_comp["modname"]] ) )
+
+# sort by offset to epoch
+data.sort(key=lambda data_tuple: data_tuple[0])
+
+# create point plot with modname annotation for every point
+offsets = [ data_tuple[0] for data_tuple in data]
+zenith_shifts = [data_tuple [1] for data_tuple in data]
+
+fig = go.Figure(data=go.Scatter(x=offsets, y=zenith_shifts, mode='lines+markers'))
+
+fig.update_layout(title_text=f'{ref_mobility}-{new_mobility} zenith shifts relative to TLE epoch at second 0')
+fig.update_xaxes(title_text='seconds to epoch')
+fig.update_yaxes(title_text='zenith shift in seconds')
+
+# estimate from just trying!
+average_pixel_width_per_char = 6
+padding = 0
+yshift_sign = 1
+
+# modname annotations
+for data_tuple in data:
+
+    modname = data_tuple[2]
+    yshift = (len(modname) * average_pixel_width_per_char / 2 + padding) * yshift_sign
+    yshift_sign *= -1
+
+    fig.add_annotation(
+        x=data_tuple[0], y=data_tuple[1],
+        text=data_tuple[2],
+        showarrow=False,
+        font=dict(size=4, color="Black"),
+        textangle=90,
+        yshift=yshift
+    )
+
+fig.write_image(args.svg_output_path, width=1920, height=1080)
+fig.write_html(args.svg_output_path.removesuffix("svg") + "html")
---
+++ scripts/plots/plot_sat_period_zenith_shifts.py
@@ -0,0 +1,48 @@
+import argparse
+import json
+import plotly.graph_objects as go
+
+parser = argparse.ArgumentParser(prog="plot_sat_period_shifts.py", 
+                                 description="""Plots zenith shifts of overlapping communication periods of two mobilities for the same satellite module
+                                 as a function of time distance to the used TLE's epoch.""")
+parser.add_argument("comm_comp_json", help="Path of the communication comparison JSON of the satellite module and the two mobilities.")
+parser.add_argument("output_path")
+args = parser.parse_args()
+
+# get data
+mobilties = args.comm_comp_json.split("/")[-1].split("_")[0].split("-")
+ref_mobility = mobilties[0]
+new_mobility = mobilties[1]
+
+with open(args.comm_comp_json, "r") as json_f:
+    comm_comp = json.load(json_f)
+
+zenith_shifts = []
+offsets_to_epoch = []
+
+for p_group in comm_comp["period_groups"]:
+    zenith_shifts.append(p_group["zenith_shift"])
+    offsets_to_epoch.append(p_group["ref_start_to_epoch_offset"])
+
+# plot data
+fig = go.Figure(data=go.Scatter(x=offsets_to_epoch, y=zenith_shifts, mode='lines+markers'))
+
+fig.update_layout(title_text=f'{ref_mobility}-{new_mobility} zenith shifts relative to TLE epoch at second 0')
+fig.update_xaxes(title_text='seconds to epoch')
+fig.update_yaxes(title_text='zenith shift in seconds')
+
+# epoch line marker and annotation
+fig.add_shape(
+    type="line",
+    x0=0, y0=min(0, min(zenith_shifts)), x1=0, y1=max(zenith_shifts),
+    line=dict(color="Red", width=1, dash="dash")
+)
+
+fig.add_annotation(
+    x=0, y=max(zenith_shifts),
+    text="epoch of satellite",
+    showarrow=False,
+    font=dict(size=14, color="Red")
+)
+
+fig.write_image(args.output_path)
