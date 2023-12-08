3107e963d9740f7689de27c9953eedcaa385920c
s3997128--tu-dresden.de <max.wendler@mailbox.tu-dresden.de>
2023-12-08 15:47:25 +0100

    removed interpolation

--- scripts/statistics/aligned_period_sop_stat_differences.py
+++ scripts/statistics/aligned_period_sop_stat_differences.py
@@ -1,7 +1,6 @@
 import argparse
 import json
 import numpy as np
-from scipy.interpolate import CubicSpline
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 
@@ -86,29 +85,68 @@ output = {"modname": comm_periods["modname"],
 for p_group_idx in range(len(period_groups)):
     
     p_group = period_groups[p_group_idx]
-    sec_range = list( range( p_group[ref_mobility]["period"][0], p_group[ref_mobility]["period"][1] + 1) )
     plot_row_idx = p_group_idx + 1
-    
+    ref_sec_range = list(range(p_group[ref_mobility]["period"][0], p_group[ref_mobility]["period"][1] + 1))
+    aligned_new_sec_range = list(range(p_group[new_mobility]["period"][0] - p_group["zenith_shift"], p_group[new_mobility]["period"][1] - p_group["zenith_shift"] + 1))
+
     # copy period start and end and mobility period idx
     new_p_group_dict = {ref_mobility: p_group[ref_mobility],
                         new_mobility: p_group[new_mobility]}
 
-    # angles
-    # interpolate new to length of ref  
+    # angles  
     ref_angles = ref_mobility_stats["angles"][ p_group[ref_mobility]["period_idx"] ]
-    ref_xs = np.linspace(0, 1, len(ref_angles))
     new_angles = new_mobility_stats["angles"][ p_group[new_mobility]["period_idx"] ]
-    new_xs = np.linspace(0, 1, len(new_angles))
 
-    angle_interpolator = CubicSpline(new_xs, new_angles)
-    new_angles_interp = angle_interpolator(ref_xs)
+    duration_difference = abs(len(ref_angles) - len(new_angles))
+    diff_start_idx = 0
+    if duration_difference % 2 == 1:
+        diff_start_idx += 1
+        duration_difference -= 1
+    diff_start_idx += int(duration_difference / 2)
+    
+    print(len(ref_angles))
+    print(len(new_angles))
+    print(diff_start_idx)
+    print(len(ref_angles) - int(duration_difference/2) + 1)
 
+    # only calc differences / changes in overlapping duration
+    # i.e. starting at idx duration_difference/2 (+1 for odd difference to len - duration_difference/2
+    
     angle_diffs_or_changes = []
-    for i in range(len(ref_angles)):
-        if args.changes:
-            angle_diffs_or_changes.append( new_angles_interp[i] - ref_angles[i] )
-        else:
-            angle_diffs_or_changes.append( abs(new_angles_interp[i] - ref_angles[i]) )
+    diff_sec_range = None
+    # fully use ref_angles, use slice of new_angles
+    if len(ref_angles) < len(new_angles):
+        aligned_overlap_new_angles = new_angles[diff_start_idx:( len(new_angles) - int(duration_difference/2))]
+        
+        # as aligned to ref_mobility period 
+        diff_sec_range = ref_sec_range        
+    
+        for i in range(len(ref_angles)):
+            if args.changes:
+                angle_diffs_or_changes.append( aligned_overlap_new_angles[i] - ref_angles[i] )
+            else:
+                angle_diffs_or_changes.append( abs(aligned_overlap_new_angles[i] - ref_angles[i]) )
+    
+    # fully use new_angles, use slice of ref_angles
+    elif len(new_angles) < len(ref_angles):
+        aligned_overlap_ref_angles = ref_angles[diff_start_idx:( len(ref_angles) - int(duration_difference/2))]
+
+        diff_sec_range = aligned_new_sec_range
+
+        for i in range(len(new_angles)):
+            if args.changes:
+                angle_diffs_or_changes.append( new_angles[i] - aligned_overlap_ref_angles[i] )
+            else:
+                angle_diffs_or_changes.append( abs(new_angles[i] - aligned_overlap_ref_angles[i]) )
+
+    # no duration difference
+    else:
+        diff_sec_range = ref_sec_range
+        for i in range(len(new_angles)):
+            if args.changes:
+                angle_diffs_or_changes.append( new_angles[i] - ref_angles[i] )
+            else:
+                angle_diffs_or_changes.append( abs(new_angles[i] - ref_angles[i]) )
 
     if args.changes:
         new_p_group_dict["angle_changes"] = angle_diffs_or_changes
@@ -116,21 +154,21 @@ for p_group_idx in range(len(period_groups)):
         new_p_group_dict["angle_differences"] = angle_diffs_or_changes
 
     fig.add_trace(
-        go.Scatter(x=sec_range, 
+        go.Scatter(x=ref_sec_range, 
                 y=ref_angles,
                 line=dict(color='Red')),
         row=plot_row_idx, col=1
     )
     
     fig.add_trace(
-        go.Scatter(x=sec_range, 
-                y=new_angles_interp,
+        go.Scatter(x=aligned_new_sec_range, 
+                y=new_angles,
                 line=dict(color='Blue')),
         row=plot_row_idx, col=1
     )
 
     fig.add_trace(
-        go.Scatter(x=sec_range, 
+        go.Scatter(x=diff_sec_range, 
                 y=angle_diffs_or_changes,
                 line=dict(color='Green')),
         row=plot_row_idx, col=1
@@ -139,20 +177,35 @@ for p_group_idx in range(len(period_groups)):
     fig.update_xaxes(title_text="sim. second", row=plot_row_idx, col=1)
     fig.update_yaxes(title_text="elevation angle in °", row=plot_row_idx, col=1)
 
-    # distances
-    # interpolate new to length of ref 
+    # distances 
     ref_distances = ref_mobility_stats["distances"][ p_group[ref_mobility]["period_idx"] ]
     new_distances = new_mobility_stats["distances"][ p_group[new_mobility]["period_idx"] ]
-
-    distance_interpolator = CubicSpline(new_xs, new_distances)
-    new_distances_interp = distance_interpolator(ref_xs)
-
+    
     distance_diffs_or_changes = []
-    for i in range(len(ref_distances)):
-        if args.changes:
-            distance_diffs_or_changes.append( new_distances_interp[i] - ref_distances[i] )
-        else:
-            distance_diffs_or_changes.append( abs(new_distances_interp[i] - ref_distances[i]) )
+    if len(ref_distances) < len(new_distances):
+        aligned_overlap_new_distances = new_distances[diff_start_idx:( len(new_distances) - int(duration_difference/2))]    
+    
+        for i in range(len(ref_distances)):
+            if args.changes:
+                distance_diffs_or_changes.append( aligned_overlap_new_distances[i] - ref_distances[i] )
+            else:
+                distance_diffs_or_changes.append( abs(aligned_overlap_new_distances[i] - ref_distances[i]) )
+    
+    elif len(new_distances) < len(ref_distances):
+        aligned_overlap_ref_distances = ref_distances[diff_start_idx:( len(ref_distances) - int(duration_difference/2))]
+
+        for i in range(len(new_distances)):
+            if args.changes:
+                distance_diffs_or_changes.append( new_distances[i] - aligned_overlap_ref_distances[i] )
+            else:
+                distance_diffs_or_changes.append( abs(new_distances[i] - aligned_overlap_ref_distances[i]) )
+
+    else:
+        for i in range(len(new_distances)):
+            if args.changes:
+                distance_diffs_or_changes.append( new_distances[i] - ref_distances[i] )
+            else:
+                distance_diffs_or_changes.append( abs(new_distances[i] - ref_distances[i]) )
 
     if args.changes:
         new_p_group_dict["distance_changes"] = distance_diffs_or_changes
@@ -160,21 +213,21 @@ for p_group_idx in range(len(period_groups)):
         new_p_group_dict["distance_differences"] = distance_diffs_or_changes
 
     fig.add_trace(
-        go.Scatter(x=sec_range, 
+        go.Scatter(x=ref_sec_range, 
                 y=ref_distances,
                 line=dict(color='Red')),
         row=plot_row_idx, col=2
     )
     
     fig.add_trace(
-        go.Scatter(x=sec_range, 
-                y=new_distances_interp,
+        go.Scatter(x=aligned_new_sec_range, 
+                y=new_distances,
                 line=dict(color='Blue')),
         row=plot_row_idx, col=2
     )
 
     fig.add_trace(
-        go.Scatter(x=sec_range, 
+        go.Scatter(x=diff_sec_range, 
                 y=distance_diffs_or_changes,
                 line=dict(color='Green')),
         row=plot_row_idx, col=2
@@ -183,20 +236,36 @@ for p_group_idx in range(len(period_groups)):
     fig.update_xaxes(title_text="sim. second", row=plot_row_idx, col=2)
     fig.update_yaxes(title_text="distance in km", row=plot_row_idx, col=2)
 
-    # delays
-    # interpolate new to length of ref 
+    # delays 
     ref_delays = ref_mobility_stats["delays"][ p_group[ref_mobility]["period_idx"] ]
     new_delays = new_mobility_stats["delays"][ p_group[new_mobility]["period_idx"] ]
 
-    delay_interpolator = CubicSpline(new_xs, new_delays)
-    new_delays_interp = delay_interpolator(ref_xs)
 
     delay_diffs_or_changes = []
-    for i in range(len(ref_delays)):
-        if args.changes:
-            delay_diffs_or_changes.append( new_delays_interp[i] - ref_delays[i] )
-        else:
-            delay_diffs_or_changes.append( abs(new_delays_interp[i] - ref_delays[i]) )
+    if len(ref_delays) < len(new_delays):
+        aligned_overlap_new_delays = new_delays[diff_start_idx:( len(new_delays) - int(duration_difference/2))]    
+    
+        for i in range(len(ref_delays)):
+            if args.changes:
+                delay_diffs_or_changes.append( aligned_overlap_new_delays[i] - ref_delays[i] )
+            else:
+                delay_diffs_or_changes.append( abs(aligned_overlap_new_delays[i] - ref_delays[i]) )
+    
+    elif len(new_delays) < len(ref_delays):
+        aligned_overlap_ref_delays = ref_delays[diff_start_idx:( len(ref_delays) - int(duration_difference/2))]
+
+        for i in range(len(new_delays)):
+            if args.changes:
+                delay_diffs_or_changes.append( new_delays[i] - aligned_overlap_ref_delays[i] )
+            else:
+                delay_diffs_or_changes.append( abs(new_delays[i] - aligned_overlap_ref_delays[i]) )
+
+    else:
+        for i in range(len(new_delays)):
+            if args.changes:
+                delay_diffs_or_changes.append( new_delays[i] - ref_delays[i] )
+            else:
+                delay_diffs_or_changes.append( abs(new_delays[i] - ref_delays[i]) )
 
     if args.changes:
         new_p_group_dict["delay_changes"] = delay_diffs_or_changes
@@ -204,21 +273,21 @@ for p_group_idx in range(len(period_groups)):
         new_p_group_dict["delay_differences"] = delay_diffs_or_changes
 
     fig.add_trace(
-        go.Scatter(x=sec_range, 
+        go.Scatter(x=ref_sec_range, 
                 y=ref_delays,
                 line=dict(color='Red')),
         row=plot_row_idx, col=3
     )
     
     fig.add_trace(
-        go.Scatter(x=sec_range, 
-                y=new_delays_interp,
+        go.Scatter(x=aligned_new_sec_range, 
+                y=new_delays,
                 line=dict(color='Blue')),
         row=plot_row_idx, col=3
     )
 
     fig.add_trace(
-        go.Scatter(x=sec_range, 
+        go.Scatter(x=diff_sec_range, 
                 y=delay_diffs_or_changes,
                 line=dict(color='Green')),
         row=plot_row_idx, col=3
