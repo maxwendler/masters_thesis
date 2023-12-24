6531070873b5499861e3f5a72537e3d1699d137b
s3997128--tu-dresden.de <max.wendler@mailbox.tu-dresden.de>
2023-12-24 20:38:55 +0100

    included effects of lost/added periods on availability changes

--- scripts/statistics/available_sats_diffs.py
+++ scripts/statistics/available_sats_diffs.py
@@ -4,14 +4,23 @@ import json
 parser = argparse.ArgumentParser()
 parser.add_argument("ref_mobility_availables_json")
 parser.add_argument("new_mobility_availables_json")
+parser.add_argument("all_interval_changes_json")
+parser.add_argument("ref_comm_periods_dir")
+parser.add_argument("new_comm_periods_dir")
 parser.add_argument("output_path")
 args = parser.parse_args()
+
+ref_comm_periods_dir = args.ref_comm_periods_dir if args.ref_comm_periods_dir.endswith("/") else args.ref_comm_periods_dir + "/"
+new_comm_periods_dir = args.new_comm_periods_dir if args.new_comm_periods_dir.endswith("/") else args.new_comm_periods_dir + "/"
 
 with open(args.ref_mobility_availables_json, "r") as json_f:
     ref_availability_stats = json.load(json_f)
 
 with open(args.new_mobility_availables_json, "r") as json_f:
     new_availability_stats = json.load(json_f)
+
+with open(args.all_interval_changes_json, "r") as json_f:
+    all_interval_changes = json.load(json_f)
 
 measurement_start_time = ref_availability_stats["available_sats"][0]["sim_time"]
 
@@ -78,11 +87,101 @@ for availability_num in new_availability_stats["availability_num_ratios"].keys()
     if availability_num not in availability_num_ratio_diffs.keys():
         availability_num_ratio_diffs[availability_num] = new_availability_stats["availability_num_ratios"][availability_num]
 
+# lost period effects
+lost_period_effects = []
+lost_periods_total_time = 0
+lost_periods_total_availability_reductions_by_one = {}
+for current_consecutive_lost_periods in all_interval_changes["consecutive_lost_periods"]:
+    for period in current_consecutive_lost_periods:
+        
+        modname = period["period_name"].split("]")[0] + "]"
+
+        with open(ref_comm_periods_dir +  modname + "_communication-periods.json", "r") as json_f:
+            mod_comm_periods = json.load(json_f)
+
+        period_times = mod_comm_periods["periods"][period["sat_period_idx"]]
+        period_duration = mod_comm_periods["durations"][period["sat_period_idx"]]
+        lost_periods_total_time += period_duration
+
+        availability_reductions_by_one = {}
+        for sim_time in range(period_times[0], period_times[1]):
+            original_availability = ref_availability_stats["available_sats"][sim_time - measurement_start_time]["sat_num"]
+            availability_reduction_key = f"{original_availability}->{original_availability - 1}"
+            if availability_reduction_key in availability_reductions_by_one.keys():
+                availability_reductions_by_one[availability_reduction_key] += 1
+            else:
+                availability_reductions_by_one[availability_reduction_key] = 1
+
+        for availability_reduction_key in availability_reductions_by_one.keys():
+            if availability_reduction_key in lost_periods_total_availability_reductions_by_one.keys():
+                lost_periods_total_availability_reductions_by_one[availability_reduction_key] += availability_reductions_by_one[availability_reduction_key]
+            else:
+                lost_periods_total_availability_reductions_by_one[availability_reduction_key] = availability_reductions_by_one[availability_reduction_key]
+
+        lost_period_effects.append({
+            "sat": modname,
+            "sat_period_idx": period["sat_period_idx"],
+            "period_seq_num": period["period_name"].split("]")[1],
+            "period": period_times,
+            "duration": period_duration,
+            "availability_reductions_by_one": availability_reductions_by_one
+        })
+
+# added periods effects
+added_period_effects = []
+added_periods_total_time = 0
+added_periods_total_availability_increases_by_one = {}
+for current_consecutive_added_periods in all_interval_changes["consecutive_added_periods"]:
+    for period in current_consecutive_added_periods:
+        
+        modname = period["period_name"].split("]")[0] + "]"
+
+        with open(new_comm_periods_dir +  modname + "_communication-periods.json", "r") as json_f:
+            mod_comm_periods = json.load(json_f)
+
+        period_times = mod_comm_periods["periods"][period["sat_period_idx"]]
+        period_duration = mod_comm_periods["durations"][period["sat_period_idx"]]
+        added_periods_total_time += period_duration
+
+        availability_increases_by_one = {}
+        for sim_time in range(period_times[0], period_times[1]):
+            original_availability = ref_availability_stats["available_sats"][sim_time - measurement_start_time]["sat_num"]
+            availability_increase_key = f"{original_availability}->{original_availability + 1}"
+            if availability_increase_key in availability_increases_by_one.keys():
+                availability_increases_by_one[availability_increase_key] += 1
+            else:
+                availability_increases_by_one[availability_increase_key] = 1
+
+        for availability_increase_key in availability_increases_by_one.keys():
+            if availability_increase_key in added_periods_total_availability_increases_by_one.keys():
+                added_periods_total_availability_increases_by_one[availability_increase_key] += availability_increases_by_one[availability_increase_key]
+            else:
+                added_periods_total_availability_increases_by_one[availability_increase_key] = availability_increases_by_one[availability_increase_key]
+
+        added_period_effects.append({
+            "sat": modname,
+            "sat_period_idx": period["sat_period_idx"],
+            "period_seq_num": period["period_name"].split("]")[1],
+            "period": period_times,
+            "duration": period_duration,
+            "availability_increases_by_one": availability_increases_by_one
+        })
+
 output = {
     "avg_abs_sat_num_diff": abs_diff_sum / len(available_sat_diffs),
     "time_with_num_diff_to_total_time": times_with_num_diff / len(available_sat_diffs),
     "availability_num_time_diffs": availability_num_time_diffs,
     "availability_num_ratio_diffs": availability_num_ratio_diffs,
+    "lost_periods_effects": {
+        "total_time": lost_periods_total_time,
+        "total_availability_reductions_by_one": lost_periods_total_availability_reductions_by_one,
+        "instances": lost_period_effects
+    },
+    "added_periods_effects": {
+        "total_time": added_periods_total_time,
+        "total_availability_increases_by_one": added_periods_total_availability_increases_by_one,
+        "instances": added_period_effects
+    },
     "available_sat_diffs": available_sat_diffs
 }
 
